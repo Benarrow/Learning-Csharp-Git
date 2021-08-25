97b1bc7e4aab84f5fe335404bd1639a4ee92908e
Benarrow <emisto@163.com>
2021-08-25 17:54:33 +0800

    All the files uploaded

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<int> AHG = new List<int> {1, 2, 3, 4};
+            foreach (int DS in AHG) {
+                Console.WriteLine(DS);
+            }
+            
+        }
+    }
+}
