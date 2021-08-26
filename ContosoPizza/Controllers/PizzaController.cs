d840e4cc2779aa6dace540b88621a5583e1a1880
Benarrow <emisto@163.com>
2021-08-26 17:56:22 +0800

    Extension of Web API

--- ContosoPizza/Controllers/PizzaController.cs
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -14,10 +14,12 @@ namespace ContosoPizza.Controllers
         {
         }
 
+        // GET all action
         [HttpGet]
         public ActionResult<List<Pizza>> GetAll() =>
             PizzaService.GetAll();
 
+        // GET by ID action
         [HttpGet("{id}")]
         public ActionResult<Pizza> Get(int id)
         {
@@ -30,9 +32,41 @@ namespace ContosoPizza.Controllers
         }
 
         // POST action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        }
 
         // PUT action
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
+
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null)
+                return NotFound();
+
+            PizzaService.Update(pizza);
+
+            return NoContent();
+        }
 
         // DELETE action
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza is null)
+                return NotFound();
+
+            PizzaService.Delete(id);
+
+            return NoContent();
+        }
     }
 }
